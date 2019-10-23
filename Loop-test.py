ae32df7333cc7a08c86c0df58869f5e295efc704
ygeleta23 <yonathan.geleta@gmail.com>
2019-10-23 14:58:00 -0700

    test

---
+++ Loop-test.py
@@ -0,0 +1,14 @@
+#Loop test
+import time
+import math 
+
+timeout = 5
+timeout_start = time.time()
+n = 0
+
+while time.time() < timeout_start + timeout:
+	if n <= 10:
+		n += 1
+		print(n)
+		#time.sleep(0.5)
+#print(n)
